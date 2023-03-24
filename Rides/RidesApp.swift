15cccf22c2c55c4f6db69538ad9fc61e7aed9187
Claudia Ton <claudia.ton@ibm.com>
2023-03-24 01:13:37 -0300

    Initial Commit

---
+++ Rides/RidesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RidesApp.swift
+//  Rides
+//
+//  Created by Claudia Ton on 2023-03-24.
+//
+
+import SwiftUI
+
+@main
+struct RidesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
